36bc05bc347ff285290b7ca71f53c3593985dcc3
Sunny G <sunny.gonna@gmail.com>
2017-05-03 10:01:47 -0600

    quick fix, bump patch version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import isolate from './isolate';
 import mapSources from './mapSources';
 import mapSinks from './mapSinks';
 import mapSourcesAndSinks from './mapSourcesAndSinks';
@@ -5,6 +6,7 @@ import mapSinksWithSources from './mapSinksWithSources';
 import combineSinks from './combineSinks';
 import combineCycles from './combineCycles';
 
+export { isolate };
 export { mapSources };
 export { mapSinks };
 export { mapSourcesAndSinks };
